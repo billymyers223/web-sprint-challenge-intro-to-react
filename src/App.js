0a3e48ea86acf3a01cb07f960c34756b6b70ffe1
billmyers223 <billymyers223@gmail.com>
2021-08-13 11:44:43 -0500

    Got program compiling and am working on app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-
+import axios from 'axios';
+import Character from './components/Character';
+//import { set } from 'msw/lib/types/context';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,10 +10,21 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState([]);
 
-  return (
+  useEffect(() =>{
+    axios.get('https://swapi.dev/api/people')
+    .then(res =>{
+      console.log(res);
+      setData(res.data.results);
+    }).catch((err) =>{console.error(err);})
+  },[])
+
+
+  return(
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character data = {data}/>
     </div>
   );
 }
