a891a7e4b4d5d369779263773096196f96456670
billmyers223 <billymyers223@gmail.com>
2021-08-13 14:45:33 -0500

    Took a break came back and got names to display on page was calling axios incorrectly, now working on styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 import Character from './components/Character';
+import styled from 'styled-components'
+
+
+const StyledApp = styled.div`
+
+  background-color: rgba(255,255,255, 0.73);
+  
+
+  width:40%;
+  height:auto;
+  margin-top:50px;
+  border-radius:15px;
+  box-shadow: 2px 2px 10px  black;
+
+  @media (max-width: 800px){
+    width:90%;
+  }
+`
+
+
 //import { set } from 'msw/lib/types/context';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,20 +32,27 @@ const App = () => {
   // sync up with, if any.
   const [data, setData] = useState([]);
 
+
   useEffect(() =>{
     axios.get('https://swapi.dev/api/people')
     .then(res =>{
-      console.log(res);
-      setData(res.data.results);
-    }).catch((err) =>{console.error(err);})
+      console.log(res.data);
+      setData(res.data);
+     
+ 
+    }).catch(err => console.error(err))
   },[])
-
-
+  
   return(
     <div className="App">
+      <StyledApp>
       <h1 className="Header">Characters</h1>
       <Character data = {data}/>
-    </div>
+      </StyledApp>
+      </div>
+      
+ 
+
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,40 @@
 // Write your Character component here
+//import { data } from 'msw/lib/types/context';
+import React from 'react';
+import styled from 'styled-components'
+
+
+const StyledApp = styled.div`
+text-align:center;
+display:flex;
+justify-content:center;
+width:100%;
+.cont{
+background-color:rgba(40,75,99,0.74);
+margin-top:20px;
+border: 3px solid #F4F9E9;
+border-radius:15px;
+}
+    h4{
+        font-size: 1.5rem;
+        color:white;
+    }
+
+`
+const Character = (props) =>{
+
+
+
+    return(
+        
+        <div>
+            <StyledApp>
+            <div>{ props.data.map(char => {
+            return <div class = 'cont'><h4>Name: {char.name} Gender: {char.gender} Birth Year: {char.birth_year}</h4></div>
+        })}
+        </div>
+        </StyledApp>
+        </div>
+    )
+}
+export default  Character;
